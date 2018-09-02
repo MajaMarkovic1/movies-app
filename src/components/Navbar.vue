47c0fbbec8835f72d157ebcc2decb51bca8edd1d
Maja Markovic <markovicm9000@gmail.com>
2018-09-02 22:23:41 +0200

    logout

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -10,19 +10,31 @@
                 to="/add">Add
             </router-link>
             <router-link 
+                v-if="!isAuthenticated"
                 class="navbar-brand" 
                 to="/login">Login
             </router-link>
+            <a href="#" v-if="isAuthenticated" class="navbar-brand" @click="logout">Logout</a>
         </nav>
     </div>
 </template>
 
 <script>
+import { authService } from '../services/Auth';
+
 export default {
     props: {
         isAuthenticated: Boolean,
 
     },
+
+    methods: {
+        logout(){
+            authService.logout()
+            this.$emit('userAuthenticated')
+            this.$router.push('/login')
+        }
+    }
 
 
 }
